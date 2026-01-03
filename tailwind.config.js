be3303718b5b8d089caae44fbce4055f586f0186
Parin SM <sm.parin@gmail.com>
2026-01-03 23:15:22 +0530

    Vite upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx,css,scss}"],
+  theme: { extend: {} },
   plugins: [],
-}
-
+};
