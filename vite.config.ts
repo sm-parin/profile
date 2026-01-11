fd0f5df372cd1762169daefd5c7dd1069651bd93
Parin SM <sm.parin@gmail.com>
2026-01-11 22:09:38 +0530

    vite update for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/sm-parin.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
