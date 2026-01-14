1acb15d1f45b6119b454643e510aafe9d4f69491
Parin SM <sm.parin@gmail.com>
2026-01-14 22:07:31 +0530

    Header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,25 +1,46 @@
-import React, { useEffect, useState } from "react";
-import styles from "./header.module.scss";
+import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import PageContainer from "../page-container";
-import { applyTheme, TTheme } from "../../../utils/functions/theme";
+import { applyTheme } from "../../../utils/functions/theme";
 import Typography from "../../library/typography";
-import { response } from "../../../data/response";
 
 const Header = ({ data }: any) => {
-  const { bg, text } = applyTheme(true);
+  const { bg, text, border } = applyTheme(true);
+  const location = useLocation();
+
+  const currentPath = location.pathname;
 
   return (
     <PageContainer
       bgColor={bg}
       className="h-12 tablet:h-15 desktop:h-18 flex justify-end items-center"
     >
-      <ul className="flex flex-row gap-4 justify-end">
-        {Object.keys(data).map((key) => (
-          <Typography variant="p1" key={key} className={`${text} capitalize`}>
-            <NavLink to={`/${key === "home" ? "" : key}`}>{key}</NavLink>
-          </Typography>
-        ))}
+      <ul className="flex">
+        {Object.keys(data).map((route, index) => {
+          const last = Object.keys(data).length - 1;
+          const path = route === "home" ? "/" : `/${route}`;
+          const isActive = currentPath === path;
+
+          return (
+            <li
+              key={route}
+              className={`
+                  ${text}
+                  capitalize
+                  px-4
+                  border-r
+                  hover:underline
+                  ${last === index ? "border-none" : ""}
+                  ${border}
+                  ${isActive ? "underline" : ""}
+                `}
+            >
+              <Typography variant="p1">
+                <NavLink to={path}>{route}</NavLink>
+              </Typography>
+            </li>
+          );
+        })}
       </ul>
     </PageContainer>
   );
