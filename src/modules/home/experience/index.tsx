253825432d4fa1b221f4392010c1c3a6d8332e43
Parin SM <sm.parin@gmail.com>
2026-01-15 12:45:59 +0530

    home experience section

--- src/modules/home/experience/index.tsx
+++ src/modules/home/experience/index.tsx
@@ -5,9 +5,11 @@ import Button from "../../../components/library/button";
 import { IHomeExperienceProps } from "./experience.model";
 import PageContainer from "../../../components/layout/page-container";
 import { applyTheme } from "../../../utils/functions/theme";
+import { useNavigate } from "react-router-dom";
 
 const HomeExperience = ({ data }: IHomeExperienceProps) => {
   const { bg, text } = applyTheme();
+  const navigate = useNavigate();
 
   const description = data.description.split("\n");
 
@@ -20,16 +22,23 @@ const HomeExperience = ({ data }: IHomeExperienceProps) => {
         {data.description}
       </Typography> */}
       <div className="flex flex-col gap-2">
-        {description.map((line: any) => (
-          <Typography className={`${text}`} variant="p2">
+        {description.map((line: string, index: number) => (
+          <Typography
+            className={`${text} ${index === 0 ? "mb-6" : ""}`}
+            variant={index === 0 ? "p2" : "p3"}
+          >
             {line}
           </Typography>
         ))}
       </div>
 
       <div className="mt-8 flex items-center gap-2 tablet:gap-4 desktop:gap-6">
-        <Button label={data.primaryCta} />
-        <Button isText label={data.secondaryCta} />
+        <Button label={data.primaryCta} onClick={() => navigate("/work")} />
+        <Button
+          isText
+          label={data.secondaryCta}
+          onClick={() => navigate("/about#contact")}
+        />
       </div>
     </PageContainer>
   );
